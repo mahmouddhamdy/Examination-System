0b1ecd97531a023133ada38ccbe6b198b8c72bcd
mahmouddhamdy <68087837+mahmouddhamdy@users.noreply.github.com>
2024-02-25 22:55:41 +0200

    Update Repo

--- Report#5.sql
+++
@@ -1,6 +0,0 @@
-CREATE PROC questionsAndChoicesFromExamID @ExamID int
-AS 
-Select distinct Questions.Content, Choices.Choice_Number, Choices.Content
-From Exams, Exam_Questions, Questions, Choices
-Where @ExamID = Exam_Questions.Exam_ID AND Exam_Questions.Question_Id = Questions.Question_Id 
-AND Questions.Question_Id = Choices.Question_Id
